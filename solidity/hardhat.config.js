96a65637060a18f0fb5c1edd7659b4b0a7aae48c
eip-toolkit <dev@example.com>
2026-09-25 08:32:07 +0800

    fix: set evmVersion=cancun for mcopy support

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -2,7 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.26",
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      evmVersion: "cancun",
+    },
+  },
   paths: {
     sources: "./contracts",
     tests: "./test",
